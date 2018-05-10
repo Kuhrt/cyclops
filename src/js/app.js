ce58962b21ac96559e0b39dc7707ceb13a1728e9
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2018-05-10 16:10:15 -0500

    Added font. Made eye follow mouse.

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,25 @@
-// JS Goes here - ES6 supported
+const eye = $(".cyclops-home-cyclops__iris");
+const iris = $(".cyclops-home-cyclops__iris img");
 
-// Say hello
-console.log("🦊 Hello! Edit me in src/js/app.js");
+if(eye.length > 0){
+
+  function move(e){
+    const offset = eye.offset();
+    const cyclopsOffset = $('.cyclops-home__cyclops').offset();
+    const x = e.pageX;
+    const y = e.pageY;
+    const left = (offset.left - x);
+    const top = (offset.top - y);
+    const rad = Math.atan2(top, left);
+
+    eye.css('transform','rotate('+rad+'rad)');
+
+    if (rad < 0) {
+      iris.css('transform','rotate('+(rad * -1)+'rad)');
+    } else {
+      iris.css('transform','rotate(-'+rad+'rad)');
+    }
+  }
+
+  $(".cyclops__home").mousemove(move);
+}
