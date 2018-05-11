5cda82c90e8b4b3a0a84475febe781e58464e9dc
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2018-05-11 14:24:38 -0500

    Made the monster shoot a laser

--- src/js/app.js
+++ src/js/app.js
@@ -6,13 +6,14 @@ const cursorDot = $('.cursor .dot');
 const cursorRing = $('.cursor .ring');
 const eye = $(".cyclops-home-cyclops__iris");
 const iris = $(".cyclops-home-cyclops__iris svg");
+const laser = $('.cyclops__laser');
 
 // MAKE THE MONSTER SHOOT A LASER
 // NOTE: Only if the mouse is still for 5 seconds
 let lastTimeMouseMoved = new Date().getTime();
 let mouseTimeout = 0;
 
-$('body').on('mousemove', function() {
+$('body').on('mousemove', function(e) {
   // Clearing the timeout if the mouse moves and setting it again in case it stops
   clearTimeout(mouseTimeout);
   setMouseTimeout();
@@ -37,8 +38,9 @@ function heatUpEye() {
   // Add a class to signify that it's heating up
   eye.addClass('heating-up');
   // Animating the iris to red
-  TweenLite.to(eye.find('#Iris'), 3, {
-    fill: '#ED6E57'
+  TweenLite.to(eye.find('#Iris'), 2, {
+    fill: '#ED6E57',
+    onComplete: fireAllLasers
   });
 }
 function coolOffEye() {
@@ -46,11 +48,53 @@ function coolOffEye() {
   if (eye.hasClass('heating-up')) {
     // Removing the class to signify it's cooling down
     eye.removeClass('heating-up');
+    // Making sure the laser is gone
+    removeLaser();
     // Turning the color back to black
-    TweenLite.to(eye.find('#Iris'), 1.5, {
+    TweenLite.to(eye.find('#Iris'), 1, {
       fill: '#2C2C2C'
     });
   }
+}
+
+function fireAllLasers() {
+  const cyclopsOffset = $('.cyclops-home__cyclops').offset();
+  const irisOffset = iris.offset();
+  const laserTopPosition = (irisOffset.top - cyclopsOffset.top) + (iris.height() / 2);
+  const laserLeftPosition = (irisOffset.left - cyclopsOffset.left) + (iris.width() / 2);
+  const cursorY = cursorDot.offset().top + cursorDot.height()/2;
+  const cursorX = cursorDot.offset().left + cursorDot.width()/2;
+  let laserLength = calculateDistance(iris, cursorX, cursorY);
+  let laserDirectionY = cursorY - (irisOffset.top + iris.height()/2);
+  let laserDirectionX = cursorX - (irisOffset.left + iris.width()/2);
+  let laserAngle = Math.atan2(laserDirectionY, laserDirectionX);
+
+  laser.css({
+    top: laserTopPosition,
+    left: laserLeftPosition,
+    transform: 'rotate('+(laserAngle + 1.5*Math.PI)+'rad)'
+  });
+
+  TweenLite.to(laser, 0.05, {
+    height: laserLength
+  });
+}
+
+function removeLaser() {
+  laser.css({
+    height: 0
+  });
+}
+
+function calculateDistance(elem, mouseX, mouseY) {
+    return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left+(elem.width()/2)), 2) + Math.pow(mouseY - (elem.offset().top+(elem.height()/2)), 2)));
+}
+
+function getDirection(x1, y1, x2, y2) {
+    // might be negative:
+    var angle = Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
+    // correct, positive angle:
+    return (angle + 360) % 360;
 }
 
 
@@ -97,7 +141,9 @@ function moveTheEye(e){
 
   if (rad < 0) {
     iris.css('transform','rotate('+(rad * -1)+'rad)');
+    // laser.css('transform','rotate('+rad+'rad)');
   } else {
     iris.css('transform','rotate(-'+rad+'rad)');
+    // laser.css('transform','rotate(-'+rad+'rad)');
   }
 }
